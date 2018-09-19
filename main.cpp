f894ca7296a3ee7dbbbfe0a30e0bd9611d3f34f6
César Salcedo <cesar.salcedo@utec.edu.pe>
2018-09-18 21:10:41 -0500

    countingSort is now implemented

--- main.cpp
+++ main.cpp
@@ -7,13 +7,13 @@ void print(int numbers[], int size);
 void countingSort(int numbers[], int size);
 
 int main(int argc, char *argv[]) {
-    int numbers[] = {1, 4, 1, 2, 7, 5, 2};
+    int numbers[] = {1, 2, 9, 9, 8, 7, 12, 6, 4, 3, 1, 2};
     int size = sizeof(numbers) / sizeof(*numbers);
 
     print(numbers, size);
     countingSort(numbers, size);
     print(numbers, size);
-    
+
     system("read");
     return EXIT_SUCCESS;
 }
@@ -26,5 +26,25 @@ void print(int numbers[], int size) {
 }
 
 void countingSort(int numbers[], int size) {
-    // TODO
+    int min = numbers[0], max = numbers[0];
+
+    for (int i = 0; i < size; i++)
+        if (numbers[i] < min)
+            min = numbers[i];
+        else if (numbers[i] > max)
+            max = numbers[i];
+
+    int *list = new int[max - min + 1] ();
+
+    for (int i = 0; i < size; i++)
+        list[numbers[i] - min]++;
+
+    for (int i = 0, j = 0; i < size; i++) {
+        while (!(list[j]--))
+            j++;
+
+        numbers[i] = j + min;
+    }
+
+    delete [] list;
 }
